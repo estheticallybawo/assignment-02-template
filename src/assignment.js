f48f42a22f3e0db6aa477374d076e135d1635a06
oluwasetemi ojo <setemiojo@gmail.com>
2025-08-26 12:37:44 -0500

    👌 IMPROVE: new changes

--- src/assignment.js
+++ src/assignment.js
@@ -1,6 +1,5 @@
 // ========================
-// DO NOT EDIT THIS BLOCK
-const assignment = {};
+// DO NOT REMOVE THE EXPORT KEYWORDS
 // ========================
 
 /**
@@ -9,35 +8,35 @@ const assignment = {};
  * Calculate and return the sum of the numbers between 1 and a destination.
  * (1 to destination) where destination is a number greater than 1 and destination is included when calculating the sum.
  *
- * If you did Challenge - 1, remove the comment in the line just after this function
  *
- * @param {Number} destination is the stopping number
+ * @param {number} destination is the stopping number
  * @returns number the sum of the numbers from 1 to destination
  */
-function sumOfNumbersTo(destination) {
-    let sum = 0;
+export function sumOfNumbersTo(destination) {
+    console.log(destination)
+    const sum = 0
+
+    // write your code here
+
     return sum
 }
-
-
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
  *
- * Calculate count, sum and put in an array the even numbers from 1 to a destination. Object keys are count, sum, arrayOfEvenNumbers.
+ * Calculate count, sum and put in an array the even numbers from 1 to a destination. The returned object should have the following keys: count, sum, arrayOfEvenNumbers.
  *
- * If you did Challenge - 2, remove the comment in the line just after this function
- *
- * @param {Number} destination the stopping number
+ * @param {number} destination the stopping number
  * @returns the object containing count, sum, arrayOfEvenNumbers from 1 to destination
  */
-function countEvenNumbersWithin(destination) {
-    // Write your code here
+export function countEvenNumbersWithin(destination) {
+    console.log(destination)
     // get the number from 1 to destination
-    let sum = 0;
-    let count = 0;
-    let arrayOfEvenNumbers = [];
+    const sum = 0
+    const count = 0
+    const arrayOfEvenNumbers = []
+
+    // write your code here
 
     return {
         // property value shorthand
@@ -45,8 +44,8 @@ function countEvenNumbersWithin(destination) {
         // you can just write the property name in your object
         count,
         sum,
-        arrayOfEvenNumbers
-    };
+        arrayOfEvenNumbers,
+    }
 }
 
 // assignment.countEvenNumbersWithin = countEvenNumbersWithin;
@@ -59,22 +58,20 @@ function countEvenNumbersWithin(destination) {
  * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  *
- * If you did Challenge - 3, remove the comment in the line just after this function
- *
  * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
  *
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    let result = [];
+export function celsiusToFahrenheit(arrayOfNumbers) {
+    console.log(arrayOfNumbers)
+    const result = []
 
-    return result;
+    // write your code here
+
+    return result
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
-
 // ========================
-// DO NOT EDIT THIS BLOCK
-module.exports = assignment;
+// DO NOT REMOVE THE EXPORT KEYWORDS
 // ========================
