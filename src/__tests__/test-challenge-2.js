8a447b88dd5c6c64992e22550f87be71910e31d7
oluwasetemi ojo <setemiojo@gmail.com>
2025-08-27 19:07:04 -0500

    fix: update test for evenNumbersWithin function to include zero in the count of even numbers

--- src/__tests__/test-challenge-2.js
+++ src/__tests__/test-challenge-2.js
@@ -2,5 +2,5 @@ import { expect, it } from 'vitest'
 import { evenNumbersWithin } from '../assignment'
 
 it('challenge - 2 : my evenNumbersWithin function can count even numbers in array of numbers', () => {
-    expect(evenNumbersWithin(10)).toStrictEqual({ count: 5, sum: 30, arrayOfEvenNumbers: [2, 4, 6, 8, 10] })
+    expect(evenNumbersWithin(10)).toStrictEqual({ count: 6, sum: 30, arrayOfEvenNumbers: [0, 2, 4, 6, 8, 10] })
 })
